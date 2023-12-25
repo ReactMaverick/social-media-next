9d6e5a6f2a08b182e0e99ad1d8f32d71286d6666
priyamwebsadroit <138439366+priyamwebsadroit@users.noreply.github.com>
2023-12-26 01:42:40 +0530

    Social Media

---
+++ components/customHead/customHead.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CustomHead() {
+    return (
+        <head>
+            <link href="bootstrap.min.css" rel="stylesheet" /> {/* Import Bootstrap CSS */}
+            <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,700i" rel="stylesheet"></link> {/* Google Font */}
+        </head>
+    )
+}
+
+export default CustomHead
