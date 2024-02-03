2315fb0c1f7537ebafb26e310b105b2e8cb6aae5
Priyam-Websadroit <priyam.websadroit@gmail.com>
2024-02-03 16:56:27 +0530

    Updated Build 03.02.2024

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
     env: {
         BASE_URL: 'http://localhost:3000',
+        userName: 'priyamwebsadroit',
+        password: 'U.-TJM_g5"J#g5N'
     },
     distDir: 'build',
 }
